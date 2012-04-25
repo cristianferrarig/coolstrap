1f77924ebc0de21515c969f6c66d26aa28624fc7
Felipe Funes <felipefunes@gmail.com>
2012-04-25 13:45:13 -0400

    new footer tabbar version

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	*/
 
 	// TODO: @abraham el iScroll no deja hacer click en un input (en lungo se puede)
-	//myScroll = new iScroll('demo');
+	myScroll = new iScroll('demo');
 
 
 });
