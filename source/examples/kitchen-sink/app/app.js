82ba96c038ae20edca35b58be8875b7b542cd98f
Cristian Ferrari <cristianferarrig@gmail.com>
2012-05-14 19:23:42 -0400

    solving problems caused by me

---
+++ source/examples/kitchen-sink/app/app.js
@@ -0,0 +1,22 @@
+var App = (function(coolstrapp, undefined) {
+
+  coolstrapp.App.init({
+    name: 'Kitchen Sink',
+    version: '1.0',
+    resources: {
+      sections: []
+    }
+  });
+
+  var _getEnvironmentFromHelper = (function() {
+    var environment = coolstrapp.Core.environment();
+    if (environment.isMobile) {
+      alert('Your phone is ' + environment.os.name + ' (' + environment.os.version + ')');
+    }
+  })();
+
+  return {
+
+  };
+
+})(STRAPP);
