308d4c05198b8b5c0836ac3b6963ec67a004b6b5
Cristian Ferrari <cristianferarrig@gmail.com>
2012-05-14 18:57:53 -0400

    try to solving varios fucking problems

--- source/examples/components/app/events.js
+++ source/examples/components/app/events.js
@@ -1,14 +1,9 @@
 App.Events = (function(coolstrapp, undefined) {
 
   $(document).ready(function() {
-<<<<<<< HEAD
 
     myScroll = new iScroll('demo');
 
-=======
-    console.log('page ready');
-    myScroll = new iScroll('demo');
->>>>>>> components example
   });
  
 
