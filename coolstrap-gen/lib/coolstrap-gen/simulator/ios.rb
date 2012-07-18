17445da8c87add2ca9de36963e5723e532f5c107
Abraham Barrera <abarrerac@gmail.com>
2012-07-18 19:18:29 -0400

    fix library and header search paths

--- coolstrap-gen/lib/coolstrap-gen/simulator/ios.rb
+++ coolstrap-gen/lib/coolstrap-gen/simulator/ios.rb
@@ -4,17 +4,17 @@ module Coolstrap::Gen
     class Ios
       class << self
         include ::Coolstrap::Gen::Utils
-        
+
         def simulate
           # 4.3 simulator: /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iOS\ Simulator.app/
           simulator_app = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\\ Simulator.app/Contents/MacOS/iPhone\\ Simulator -SimulateApplication"
-          app_path = "source/native/ios/build/Release-iphonesimulator/NativeBridgeiOS.app/NativeBridgeiOS"
+          app_path = "source/native/ios/build/Release-iphonesimulator/KitchenSink.app/KitchenSink"
           system "echo :::LAUNCHING IPHONE SIMULATOR:::"
           # We need to use the session gem so that we can access the user's aliases
           bash = Session::Bash::new 'program' => 'bash --login -i'
           cmd = "#{simulator_app} #{app_path}"
           #system cmd # ok
-          #bash.execute(cmd) 
+          #bash.execute(cmd)
           bash.execute(cmd) { |out, err| puts out }
         end
 
