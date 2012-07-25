d9d7376350ffbc6a918c3a6fab1e535a0a1316d2
Cristian Ferrari <cristianferrarig@gmail.com>
2012-07-25 11:01:07 -0400

    some css fix and start with kitchen sink project

---
+++ coolstrap-core/spec/fixtures/kitchen-sink/source/native/ios/KitchenSink/Coolstrap.framework/Versions/A/Headers/CsCoolWebView.h
@@ -0,0 +1,13 @@
+//
+//  CsCoolWebView.h
+//  Coolstrap
+//
+//  Created by Abraham Barrera on 7/18/12.
+//  Copyright (c) 2012 Rhyboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CsCoolWebView : UIWebView
+
+@end
